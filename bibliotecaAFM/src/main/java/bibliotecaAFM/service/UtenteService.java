881263c3f6f610c6b80e38395c570473dc02529e
Andrea D'ottavio <andreadottavio70@gmail.com>
2020-06-18 11:58:01 +0200

    UtenteService

---
+++ bibliotecaAFM/src/main/java/bibliotecaAFM/service/UtenteService.java
@@ -0,0 +1,19 @@
+package bibliotecaAFM.service;
+
+import java.util.List;
+
+import bibliotecaAFM.model.Utente;
+
+public interface UtenteService {
+	public void addUtente(Utente u);
+
+	public void deleteUtente(Long id);
+
+	public void updateUtente(Utente u);
+
+	public List<Utente> findAllUtente();
+
+	public List<Utente> findAllUtenteById(Long id);
+
+	public List<Utente> findAllUtenteByEmail(String email);
+}
