5a5d6935cfda7097b9cbd6abf0575ac6ae59ab71
matteo <saccone.matteo.io@gmail.com>
2020-06-18 12:05:20 +0200

    update utenteService

--- bibliotecaAFM/src/main/java/bibliotecaAFM/service/UtenteService.java
+++ bibliotecaAFM/src/main/java/bibliotecaAFM/service/UtenteService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import bibliotecaAFM.model.Utente;
 
 public interface UtenteService {
+	
 	public void addUtente(Utente u);
 
 	public void deleteUtente(Long id);
@@ -16,6 +17,6 @@ public interface UtenteService {
 	public List<Utente> findAllUtenteById(Long id);
 
 	public List<Utente> findAllUtenteByEmail(String email);
-	
-	public Utente login (String password, String email);
+
+	public Utente login(String password, String email);
 }
